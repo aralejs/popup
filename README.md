ff232800c7550bf2f71305cf61811fd3a6878028
popomore <sakura9515@gmail.com>
2012-06-16 21:48:46 +0800

    Triggerable 改名为 Popup

---
+++ README.md
@@ -0,0 +1,44 @@
+
+# Popup
+
+Popup 是可触发 Overlay 型 UI 组件。
+
+---
+
+
+## 依赖
+
+ - [seajs](seajs/README.md)
+ - [position](position/README.md)
+ - [overlay](overlay/README.md)
+
+
+## 使用说明
+
+
+### Dropdown
+
+```
+seajs.use(['jquery','dropdown'], function($, Dropdown){
+    var example = new Dropdown({
+        trigger: '#triggerId',
+        element: '#targetId'
+    });
+});
+```
+
+
+## 演示页面
+
+更多用法，请看 Demo 中的例子：
+* [http://aralejs.org/lib/dropdown/examples/dropdown.html](http://aralejs.org/lib/dropdown/examples/dropdown.html)
+
+
+## 单元测试
+
+* [tests/runner.html](http://aralejs.org/lib/dropdown/tests/runner.html)
+
+
+## 感谢
+
+* Bootstrap Dropdown
---
+++ tests/dropdown-spec.js
@@ -0,0 +1,82 @@
+define(function(require) {
+
+    var $ = require('jquery');
+    var Dropdown = require('../src/dropdown');
+
+    describe('dropdown', function() {
+        var element = '<div>' +
+                            '<a href="#" id="trigger1">dropdown</a>' +
+                            '<ul id="element1">' +
+                                '<li>那些年，我们一起写过的单元测试...</li>' +
+                                '<li>卖萌是一种风格...</li>' +
+                            '</ul>' +
+                      '</div>';
+        beforeEach(function() {
+            element = $(element).appendTo(document.body);
+        });
+
+        afterEach(function() {
+            element.remove();
+        });
+
+        test('instance', function() {
+            var test1 = new Dropdown({
+                trigger: '#trigger1',
+                element: '#element1',
+                timeout: 200
+            });
+            var trigger = test1.get('trigger');
+            var align = test1.get('align');
+            expect(trigger.attr('id')).toBe('trigger1');
+            expect(test1.element.attr('id')).toBe('element1');
+            expect(test1.get('triggerType')).toBe('hover');
+            expect(test1.get('timeout')).toBe(200);
+            expect(test1.get('delay')).toBe(100);
+            expect($(align.baseElement)[0]).toBe(trigger[0]);
+            expect(align.baseXY).toEqual([0,'100%']);
+            expect(align.selfXY).toEqual([0,0]);
+        });
+
+        test('event', function() {
+            var event1;
+            var event2;
+            var test2 = new Dropdown({
+                trigger: '#trigger1',
+                element: '#element1'
+            });
+
+            var showText = 'test2 is shown';
+            var hideText = 'test2 is hidden';
+            // 订阅事件
+            test2.on('shown', function() {
+                event1 = showText;
+            }).on('hidden', function() {
+                event2 = hideText;
+            });
+            // 发布事件
+            test2.trigger('shown').trigger('hidden');
+            // 测试值
+            expect(event1).toBe(showText);
+            expect(event2).toBe(hideText);
+
+            // 鼠标移入
+            $('#trigger1').trigger('mouseover');
+            test2.after('show', function(){
+                expect(test2.get('trigger').is(':visible')).toBe(true);
+                expect(test2.element.is(':visible')).toBe(true);
+            });
+
+            // 鼠标移出
+            $('#trigger1').trigger('mouseout');
+
+            test2.after('hide', function() {
+                expect(test2.get('trigger').is(':visible')).toBe(true);
+                expect(test2.element.is(':hidden')).toBe(true);
+            });
+
+        });
+
+
+    });
+
+});
