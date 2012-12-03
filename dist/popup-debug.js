2c3d8d122144eef717725b325a885bd0da3e8c83
pianyou <afcstar@163.com>
2012-12-03 15:30:23 +0800

    build 0.9.10-dev

--- dist/popup-debug.js
+++ dist/popup-debug.js
@@ -1,7 +1,7 @@
-define("arale/popup/0.9.9/popup-debug", ["$-debug", "arale/overlay/0.9.12/overlay-debug", "arale/position/1.0.0/position-debug", "arale/iframe-shim/1.0.0/iframe-shim-debug", "arale/widget/1.0.2/widget-debug", "arale/base/1.0.1/base-debug", "arale/class/1.0.0/class-debug", "arale/events/1.0.0/events-debug"], function(require, exports, module) {
+define("arale/popup/0.9.9/popup-debug", ["$-debug", "arale/overlay/0.9.13/overlay-debug", "arale/position/1.0.0/position-debug", "arale/iframe-shim/1.0.0/iframe-shim-debug", "arale/widget/1.0.2/widget-debug", "arale/base/1.0.1/base-debug", "arale/class/1.0.0/class-debug", "arale/events/1.0.0/events-debug"], function(require, exports, module) {
 
     var $ = require('$-debug');
-    var Overlay = require('arale/overlay/0.9.12/overlay-debug');
+    var Overlay = require('arale/overlay/0.9.13/overlay-debug');
 
 
     // Popup 是可触发 Overlay 型 UI 组件
