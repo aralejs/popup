d23ede673d87ae43f30a28f1208a02921df72a8e
偏右 <afcstar@163.com>
2013-01-09 20:22:01 +0800

    完善用例

--- tests/popup-spec.js
+++ tests/popup-spec.js
@@ -11,69 +11,121 @@ define(function(require) {
                                 '<li>卖萌是一种风格...</li>' +
                             '</ul>' +
                       '</div>';
+        var pop;
+
         beforeEach(function() {
             element = $(element).appendTo(document.body);
         });
 
         afterEach(function() {
+            pop && pop.destroy();
+            pop = null;
             element.remove();
         });
 
         it('instance', function() {
-            var test1 = new Popup({
+            pop = new Popup({
                 trigger: '#trigger1',
                 element: '#element1'
             });
-            var trigger = test1.get('trigger');
-            var align = test1.get('align');
+            var trigger = pop.get('trigger');
+            var align = pop.get('align');
             expect(trigger.attr('id')).to.be('trigger1');
-            expect(test1.element.attr('id')).to.be('element1');
-            expect(test1.get('triggerType')).to.be('hover');
-            expect(align.baseXY).to.eql([0,'100%']);
-            expect(align.selfXY).to.eql([0,0]);
+            expect(pop.element.attr('id')).to.be('element1');
+            expect(pop.get('triggerType')).to.be('hover');
+            expect(align.baseXY).to.eql([0, '100%']);
+            expect(align.selfXY).to.eql([0, 0]);
         });
 
-        it('event', function() {
+        it('hover event', function() {
             var event1;
             var event2;
-            var test2 = new Popup({
+            pop = new Popup({
                 trigger: '#trigger1',
                 element: '#element1'
             });
 
-            var showText = 'test2 is shown';
-            var hideText = 'test2 is hidden';
+            var showText = 'pop is shown';
+            var hideText = 'pop is hidden';
             // 订阅事件
-            test2.on('shown', function() {
+            pop.on('shown', function() {
                 event1 = showText;
             }).on('hidden', function() {
                 event2 = hideText;
             });
             // 发布事件
-            test2.trigger('shown').trigger('hidden');
+            pop.trigger('shown').trigger('hidden');
             // 测试值
             expect(event1).to.be(showText);
             expect(event2).to.be(hideText);
 
             // 鼠标移入
             $('#trigger1').trigger('mouseover');
-            test2.after('show', function(){
-                expect(test2.get('trigger').is(':visible')).to.be(true);
-                expect(test2.element.is(':visible')).to.be(true);
+            pop.after('show', function() {
+                expect(pop.get('trigger').is(':visible')).to.be(true);
+                expect(pop.element.is(':visible')).to.be(true);
             });
 
             // 鼠标移出
             $('#trigger1').trigger('mouseout');
 
-            test2.after('hide', function() {
-                expect(test2.get('trigger').is(':visible')).to.be(true);
-                expect(test2.element.is(':hidden')).to.be(true);
+            pop.after('hide', function() {
+                expect(pop.get('trigger').is(':visible')).to.be(true);
+                expect(pop.element.is(':hidden')).to.be(true);
             });
 
         });
 
+        it('click event', function() {
+            pop = new Popup({
+                trigger: '#trigger1',
+                element: '#element1',
+                triggerType: 'click'
+            });
+            pop.render();
+            expect(pop.element.is(':visible')).to.be(false);            
+            $('#trigger1').click();
+            expect(pop.element.is(':visible')).to.be(true);
+            $('#trigger1').click();
+            expect(pop.element.is(':visible')).to.be(false);
+        });
+
+        it('focus & blur event', function(done) {
+            var input = $('<input type="text" />');
+            input.appendTo(document.body);
+            pop = new Popup({
+                trigger: 'input',
+                element: '#element1',
+                triggerType: 'focus'
+            });
+            pop.render();
+            expect(pop.element.is(':visible')).to.be(false);
+            input[0].focus();
+            expect(pop.element.is(':visible')).to.be(true);
+            input[0].blur();
+            setTimeout(function() {
+                expect(pop.element.is(':visible')).to.be(false);
+                input.remove();
+                done();
+            }, 100);
+        });
+
+        it('delay = -1', function() {
+            pop = new Popup({
+                trigger: '#trigger1',
+                element: '#element1',
+                delay: -1
+            });
+            pop.render();
+            expect(pop.element.is(':visible')).to.be(false);
+            $('#trigger1').trigger('mouseover');
+            expect(pop.element.is(':visible')).to.be(true);
+            $('#trigger1').trigger('mouseout');
+            expect(pop.element.is(':visible')).to.be(false);
+        });
+
         it('change align XY', function() {
-            var test1 = new Popup({
+            pop = new Popup({
                 trigger: '#trigger1',
                 element: '#element1',
                 align: {
@@ -81,48 +133,66 @@ define(function(require) {
                     selfXY: [34, 120]
                 }
             });
-            var align = test1.get('align');
+            var align = pop.get('align');
             expect(align.baseXY).to.eql(['40%', 0]);
             expect(align.selfXY).to.eql([34, 120]);
         });
 
         it('change align baseElement', function() {
-            var test1 = new Popup({
+            var pop = new Popup({
                 trigger: '#trigger1',
                 element: '#element1',
                 align: {
                     baseElement: 'body'
                 }
             });
-            var align = test1.get('align');
+            var align = pop.get('align');
             expect(align.baseElement).to.eql('body');
         });
 
         it('change align baseElement after show', function() {
-            var test1 = new Popup({
+            var pop = new Popup({
                 trigger: '#trigger1',
                 element: '#element1',
                 align: {
                     baseElement: 'body'
                 }
             });
-            test1.show();
-            var align = test1.get('align');
+            pop.show();
+            var align = pop.get('align');
             expect(align.baseElement).to.eql('body');
         });
 
         it('change specified BaseElement', function() {
-            var test1 = new Popup({
+            var pop = new Popup({
                 trigger: '#trigger1',
                 element: '#element1'
             });
-            expect(test1.get('align').baseElement._id).to.be('VIEWPORT');            
-            expect(test1._specifiedBaseElement).to.be(undefined);            
-            test1.set('align', {
+            expect(pop.get('align').baseElement._id).to.be('VIEWPORT');
+            expect(pop._specifiedBaseElement).to.be(undefined);
+            pop.set('align', {
                 baseElement: 'body'
             });
-            expect(test1._specifiedBaseElement).to.be(true);
-            expect(test1.get('align').baseElement).to.be('body');
+            expect(pop._specifiedBaseElement).to.be(true);
+            expect(pop.get('align').baseElement).to.be('body');
+        });
+
+        it('disable', function() {
+            pop = new Popup({
+                trigger: '#trigger1',
+                element: '#element1',
+                triggerType: 'click',
+                disabled: true
+            });
+            pop.render();
+            expect(pop.element.is(':visible')).to.be(false);
+            $('#trigger1').click();
+            expect(pop.element.is(':visible')).to.be(false);
+            pop.set('disabled', false);
+            $('#trigger1').click();
+            expect(pop.element.is(':visible')).to.be(true);
+            $('#trigger1').click();
+            expect(pop.element.is(':visible')).to.be(false);
         });
 
     });
